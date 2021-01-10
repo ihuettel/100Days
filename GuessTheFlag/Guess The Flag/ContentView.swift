d452d337602f1ad2f7cc48624aedd5503d0bc867
ihuettel <ian.huettel@gmail.com>
2021-01-10 12:06:21 -0500

    Created initial files for GuessTheFlag (no actual content yet)

---
+++ GuessTheFlag/Guess The Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess The Flag
+//
+//  Created by homebase on 1/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/README.md
@@ -0,0 +1,6 @@
+# Guess the Flag
+
+This is the third project in Paul Hudson's 100 Days of SwiftUI series.
+It involves building a game that asks players to guess which of three flags belongs to a given country.
+
+Building this teaches more introductory SwiftUI content such as stacks (VStack, HStack, ZStack), Image, and Alert.
