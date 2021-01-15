d9bf99cf437840295abc5b485b63335dfed2a279
ihuettel <ian.huettel@gmail.com>
2021-01-15 01:50:25 -0500

    Polished UI a bit. Editted tests (see comments for details.)

--- BetterRest/BetterRestUITests/BetterRestUITests.swift
+++ BetterRest/BetterRestUITests/BetterRestUITests.swift
@@ -42,7 +42,7 @@ class BetterRestUITests: XCTestCase {
         for _ in 0 ... 10 {
             incrementButton.doubleTap()
         }
-        XCTAssertEqual(app.staticTexts["coffee"].label, "20 cups of coffee", "Error incrementing Coffee stepper up to 20.")
+        XCTAssertEqual(app.staticTexts["20 cups of coffee"].label, "20 cups of coffee", "Error incrementing Coffee stepper up to 20.")
     }
 
     func testCoffeeDecrement() throws {
@@ -50,7 +50,19 @@ class BetterRestUITests: XCTestCase {
         for _ in 0 ... 10 {
             decrementButton.doubleTap()
         }
-        XCTAssertEqual(app.staticTexts["coffee"].label, "0 cups of coffee", "Error decrementing Coffee stepper down to 0.")
+        XCTAssertEqual(app.staticTexts["0 cups of coffee"].label, "0 cups of coffee", "Error decrementing Coffee stepper down to 0.")
     }
+    
+    //
+    //  Both testCoffee[...] tests above used to use app.staticTexts["coffee"] until the identifier broke while messing with the UI.
+    //  I've toyed around with it, but at the exact thing works above and they're within the same Form/VStack structure, I'm unsure
+    //  what could be causing it.
+    //
+    //  For now, I'm leaving it as is. These tests are to help me learn more about testing, and clearly something here is flaky.
+    //  ..it's probably that I'm doing UI testing and know very little, therefore I write flaky UI tests..
+    //  I'm honestly surprised how well the coordinate workaround functions. Shoutout to Rob Sturgeon for his article on Medium.
+    //  [https://medium.com/twinkl-educational-publishers/accessible-swiftui-for-easy-ui-testing-f2e7b8824b39]
+    //  Really helped me fix a lot of basic little things in my UI Tests that weren't working or were inefficient.
+    //
 
 }
