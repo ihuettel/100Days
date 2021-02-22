bd22e4cc5e2a2ca44fd4041d2b4480fdc43269f5
ihuettel <ian.huettel@gmail.com>
2021-02-21 23:19:24 -0500

    Added unit test for bundle extension

--- Moonshot/Shared/Bundle-DecodeExtension.swift
+++ Moonshot/Shared/Bundle-DecodeExtension.swift
@@ -9,6 +9,20 @@ import Foundation
 
 extension Bundle {
     func decode(_ file: String) -> [Astronaut] {
-        return []
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to get \(file) data from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let decoded = try? decoder.decode([Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) data from bundle.")
+        }
+        
+        return decoded
     }
 }
--- Moonshot/Tests iOS/Tests_iOS.swift
+++ Moonshot/Tests iOS/Tests_iOS.swift
@@ -6,16 +6,12 @@
 //
 
 import XCTest
+@testable import Moonshot
 
 class Tests_iOS: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
@@ -23,8 +19,9 @@ class Tests_iOS: XCTestCase {
     }
     
     func testBundleDecodeAstronauts() throws {
-        let astronauts = Bundle.main.decode("astronauts.json")
-        XCTAssertEqual(astronauts.count, 32, "Expected 32 astronauts, not \(astronauts.count)")
+        let bundle = Bundle(for: type(of: self)) // Load the proper bundle so we can find astronauts.json in shared resources
+        let astronauts: [Astronaut] = bundle.decode("astronauts.json") // Use decode extension on the bundle we found
+        XCTAssertEqual(astronauts.count, 32, "Expected 32 astronauts, not \(astronauts.count)") // Make sure the extension worked
     }
 
     func testExample() throws {
